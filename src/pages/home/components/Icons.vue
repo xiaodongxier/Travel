3a7b311bb95d33167f89eb860a41b580d0b959cf
王永杰 <admin@xiaodongxier.com>
2020-08-04 16:55:02 +0800

    增加 icons 布局

---
+++ src/pages/home/components/Icons.vue
@@ -0,0 +1,95 @@
+<template>
+  <div class="icons">
+    <div class="icon">
+      <div class="icon-img">
+        <img class="icon-img-content" src="https://s.qunarzz.com/homenode/images/touchheader/hotel.png" alt="">
+      </div>
+      <p class="icon-desc">热门景点</p>
+    </div>
+    <div class="icon">
+      <div class="icon-img">
+        <img class="icon-img-content" src="http://s.qunarzz.com/homenode/images/touchheader/flight.png" alt="">
+      </div>
+      <p class="icon-desc">热门景点</p>
+    </div>
+    <div class="icon">
+      <div class="icon-img">
+        <img class="icon-img-content" src="http://s.qunarzz.com/homenode/images/touchheader/train.png" alt="">
+      </div>
+      <p class="icon-desc">热门景点</p>
+    </div>
+    <div class="icon">
+      <div class="icon-img">
+        <img class="icon-img-content" src="http://s.qunarzz.com/homenode/images/touchheader/package.png" alt="">
+      </div>
+      <p class="icon-desc">热门景点</p>
+    </div>
+    <div class="icon">
+      <div class="icon-img">
+        <img class="icon-img-content" src="https://s.qunarzz.com/homenode/images/touchheader/hotel.png" alt="">
+      </div>
+      <p class="icon-desc">热门景点</p>
+    </div>
+    <div class="icon">
+      <div class="icon-img">
+        <img class="icon-img-content" src="http://s.qunarzz.com/homenode/images/touchheader/flight.png" alt="">
+      </div>
+      <p class="icon-desc">热门景点</p>
+    </div>
+    <div class="icon">
+      <div class="icon-img">
+        <img class="icon-img-content" src="http://s.qunarzz.com/homenode/images/touchheader/train.png" alt="">
+      </div>
+      <p class="icon-desc">热门景点</p>
+    </div>
+    <div class="icon">
+      <div class="icon-img">
+        <img class="icon-img-content" src="http://s.qunarzz.com/homenode/images/touchheader/package.png" alt="">
+      </div>
+      <p class="icon-desc">热门景点</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default{
+  name: 'HomeIcons'
+}
+</script>
+
+<style lang="stylus" scoped>
+@import '~styles/varibles.styl';
+.icons
+  width 100%
+  height 0
+  overflow hidden
+  padding-bottom 50%
+  .icon
+    width 25%
+    height 0
+    padding-bottom 25%
+    float left
+    overflow hidden
+    position relative
+    .icon-img
+      position absolute
+      top 0
+      left 0
+      right 0
+      bottom .44rem
+      box-sizing border-box
+      padding .1rem
+      .icon-img-content
+        display block
+        height 100%
+        margin 0 auto
+    .icon-desc
+      position absolute
+      left 0
+      right 0
+      bottom 0
+      height .44rem
+      line-height .44rem
+      text-align center
+      color $darkTextColor
+</style>
