4ff119697c8682b7e154ee7d58f5f0382f679fb2
王永杰 <admin@xiaodongxier.com>
2020-08-05 09:53:55 +0800

    开发错分子切换回来

---
+++ src/pages/home/components/Icons.vue
@@ -0,0 +1,105 @@
+<template>
+  <div class="icons">
+    <swiper>
+      <swiper-slide>
+        <div class="icon" v-for="item of iconList" :key="item.id">
+          <div class="icon-img">
+            <img class="icon-img-content" :src="item.imgUrl" alt="">
+          </div>
+          <p class="icon-desc">{{item.desc}}</p>
+        </div>
+      </swiper-slide>
+    </swiper>
+  </div>
+</template>
+
+<script>
+export default{
+  name: 'HomeIcons',
+  data: function () {
+    return {
+      iconList: [{
+        id: '0001',
+        imgUrl: 'http://oss.xiaodongxier.com/gome/test/piao.png',
+        desc: '景点门票'
+      },
+      {
+        id: '0002',
+        imgUrl: 'http://oss.xiaodongxier.com/gome/test/train.png',
+        desc: '滑雪集结'
+      },
+      {
+        id: '0003',
+        imgUrl: 'http://oss.xiaodongxier.com/gome/test/hotel.png',
+        desc: '泡温泉'
+      },
+      {
+        id: '0004',
+        imgUrl: 'http://oss.xiaodongxier.com/gome/test/train.png',
+        desc: '动植物园'
+      },
+      {
+        id: '0005',
+        imgUrl: 'http://oss.xiaodongxier.com/gome/test/package.png',
+        desc: '景点门票'
+      },
+      {
+        id: '0006',
+        imgUrl: 'http://oss.xiaodongxier.com/gome/test/flight.png',
+        desc: '滑雪集结'
+      },
+      {
+        id: '0007',
+        imgUrl: 'http://oss.xiaodongxier.com/gome/test/train.png',
+        desc: '泡温泉'
+      },
+      {
+        id: '0008',
+        imgUrl: 'http://oss.xiaodongxier.com/gome/test/package.png',
+        desc: '动植物园'
+      },
+      {
+        id: '0009',
+        imgUrl: 'http://oss.xiaodongxier.com/gome/test/flight.png',
+        desc: '动植物园'
+      }]
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+@import '~styles/varibles.styl';
+.icons >>> .swiper-container
+  width 100%
+  height 0
+  padding-bottom 50%
+.icon
+  width 25%
+  height 0
+  padding-bottom 25%
+  float left
+  overflow hidden
+  position relative
+  .icon-img
+    position absolute
+    top 0
+    left 0
+    right 0
+    bottom .44rem
+    box-sizing border-box
+    padding .1rem
+    .icon-img-content
+      display block
+      height 100%
+      margin 0 auto
+  .icon-desc
+    position absolute
+    left 0
+    right 0
+    bottom 0
+    height .44rem
+    line-height .44rem
+    text-align center
+    color $darkTextColor
+</style>
---
+++ src/pages/home/components/Swiper.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="wrapper">
+    <swiper :options="swiperOptions">
+      <swiper-slide v-for="item of swiperList" :key="item.id">
+        <img class="swiper-img" :src="item.imgUrl" alt="">
+      </swiper-slide>
+      <div class="swiper-pagination" slot="pagination"></div>
+    </swiper>
+  </div>
+</template>
+
+<script>
+export default{
+  name: 'HomeSwiper',
+  // data: function(){
+  data () {
+    return {
+      swiperOptions: {
+        pagination: '.swiper-pagination',
+        loop: true
+        // pagination: {
+        //   el: '.swiper-pagination'
+        // }
+      },
+      swiperList: [{
+        id: '001',
+        imgUrl: 'http://oss.xiaodongxier.com/gome/test/20200501.jpg'
+      },
+      {
+        id: '002',
+        imgUrl: 'http://oss.xiaodongxier.com/gome/test/20200502.jpg'
+      },
+      {
+        id: '003',
+        imgUrl: 'http://oss.xiaodongxier.com/gome/test/20200503.jpg'
+      }]
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+.wrapper >>> .swiper-pagination-bullet-active
+    background #fff
+.wrapper
+  width 100%
+  height 0
+  overflow hidden
+  padding-bottom 56.26%
+  // padding-bottom 31.25%
+  background #eee
+  .swiper-img
+    width 100%
+</style>
