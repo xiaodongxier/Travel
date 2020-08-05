bd13c8a72d717c5e410c29cdc953259bbb68f538
王永杰 <admin@xiaodongxier.com>
2020-08-05 10:06:31 +0800

    解决冲突

--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -1,15 +1,21 @@
 <template>
   <div>
     <HomeHeader></HomeHeader>
+    <HomeSwiper></HomeSwiper>
+    <HomeIcons></HomeIcons>
   </div>
 </template>
 
 <script>
 import HomeHeader from './components/Header'
+import HomeSwiper from './components/Swiper'
+import HomeIcons from './components/Icons'
 export default {
   name: 'Home',
   components: {
-    HomeHeader
+    HomeHeader,
+    HomeSwiper,
+    HomeIcons
   }
 }
 </script>
