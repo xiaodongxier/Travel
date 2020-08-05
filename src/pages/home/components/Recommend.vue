3a05d6cb12a235c07b05a444e8e74f5bd566bcc7
王永杰 <admin@xiaodongxier.com>
2020-08-05 17:59:07 +0800

    增加 热销推荐

---
+++ src/pages/home/components/Recommend.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <div class="recommend">热销推荐</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeRecommend'
+}
+</script>
+
+<style lang="stylus" scoped>
+
+</style>
